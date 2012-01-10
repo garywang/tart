45befb72278a7eb470ad48bb5faa58582bad4c94
Gary Wang <garywang@mit.edu>
2012-01-10 02:05:26 -0500

    Add RealCamera

--- tart/sensors/camera.py
+++ tart/sensors/camera.py
@@ -1,12 +1,8 @@
 import cv
+import threading
 
-__all__ = ['Camera', 'FileCamera']
-
-class Camera(object):
-    """A class that represents sources of images"""
-
-class FileCamera(Camera):
-    """A Camera that reads images from files"""
+class FileCamera():
+    """A "camera" that reads images from files"""
     
     def __init__(self, file):
         self.file=file
@@ -15,3 +11,37 @@ class FileCamera(Camera):
         im=cv.LoadImage(self.file, iscolor=cv.CV_LOAD_IMAGE_COLOR)
         return im
 
+class RealCamera():
+    """An actual camera"""
+    
+    def __init__(self, num):
+        self.capture=cv.CaptureFromCAM(num)
+    
+    def get_image(self):
+        return cv.QueryFrame(self.capture)
+
+class WrapperCamera(threading.Thread):
+    """Continuously call get_image of wrapped camera, return latest result"""
+    
+    def __init__(self, cam):
+        threading.Thread.__init__(self)
+        self.cam=cam
+        self.lock=threading.Lock()
+        self.start()
+    
+    def run(self):
+        self.running=True
+        while self.running:
+            #self.lock.acquire()
+            self.image=self.cam.get_image()
+            #self.lock.release()
+    
+    def get_image(self):
+        #self.lock.acquire()
+        im=cv.CreateImage((self.image.width, self.image.height), cv.IPL_DEPTH_8U, 3)
+        cv.Copy(self.image, im)
+        #self.lock.release()
+        return im
+    
+    def stop(self):
+        self.running=False
