c24efc030920432b3d5734f1d676dd2038d7ac37
Gary Wang <garywang@mit.edu>
2012-01-13 01:25:23 -0500

    Remove CameraInfo from vision_test

--- tart/tests/vision_test.py
+++ tart/tests/vision_test.py
@@ -5,8 +5,7 @@ from tart.sensors import camera, vision, trig
 if __name__=="__main__":
     try:
         #cam=camera.WebCam()
-        info=trig.CameraInfo(cam_height=29., height_angle=0.70, width_angle=0.93, min_dist=28.)
-        vt=vision.VisionThread(info, debug=True)
+        vt=vision.VisionThread(debug=True)
         vt.start()
         time.sleep(100)
     except KeyboardInterrupt:
