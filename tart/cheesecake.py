f4ab71a1cced41e6ce781d14ca29a4de7f7c411c
Gary Wang <garywang@mit.edu>
2012-01-17 02:47:22 -0500

    Cheesecake starts mapping

--- tart/cheesecake.py
+++ tart/cheesecake.py
@@ -2,35 +2,35 @@ import sys, time
 sys.path.append("/home/maslab-team-5/Maslab/tart/Libraries/")
 import arduino
 from tart.logic import state_machine
-from tart.world import vision
-from tart.control import pidrive
+from tart.world import mapping
+#from tart.control import piddrive
 from tart.actuators import drive
 
 class Cheesecake:
     def __init__(self):
         self.ard = arduino.ArduinoThread()
-        self.vis = vision.VisionThread(self)
-        #self.map = mapping.MappingThread(self) # not implemented. notes: arguments
+        self.map = mapping.MappingThread()
         self.sm = state_machine.StateMachine(self)
-	self.ctl = control.PIDriveThread(self)
+	#self.ctl = piddrive.PIDDriveThread(self)
 	#self.cam = camera
 	#self.bump = bump
         #self.dt = drive.OmniDrive(self.ard)
     
     def start(self):
         ard.start()
-        vis.start()
+        map.start()
         sm.start()
     
     def stop(self):
         ard.stop()
-        vis.stop()
+        map.stop()
         sm.stop()
 
 if __name__ == "__main__":
     try:
-        bot = Robot()
+        bot = Cheesecake()
         bot.start()
+        time.sleep(3*60+5)
     
     except KeyboardInterrupt:
         print "Ending Program"
