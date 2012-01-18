8f74cbaeee1da02fdb4580fe85e869c3b50b612b
Gary Wang <garywang@mit.edu>
2012-01-17 20:48:29 -0500

    Basic P-control

--- tart/control/pidrive.py
+++ tart/control/pidrive.py
@@ -1,15 +1,20 @@
-import sys, time
+import sys, time, math
 import threading, thread
 from tart.actuators import drive
 
 class PIDriveController(drive.OmniDrive):
     def __init__(self, ard, map):
         drive.OmniDrive.__init__(self, ard)
+        self.map=map
     
     def drive_to_point(self, point):
         """Use closed loop control to drive to a point"""
-        pass
+        dx, dy=self.map.get_vector_to(point)
+        theta=math.pi/2-math.atan2(dy, dx)
+        self.forward(100*theta)
 
     def rotate_toward_point(self, point):
         """Use closed loop control to rotate in place in direction of point"""
-        pass
+        dx, dy=self.map.get_vector_to(point)
+        theta=math.pi/2-math.atan2(dy, dx)
+        self.rotate(100*theta)
