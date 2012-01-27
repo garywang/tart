13d2bc6fb547204914c5336d341e56b8bdbda842
Gary Wang <garywang@mit.edu>
2012-01-27 14:45:09 -0500

    Add test for HMotor, fix syntax problems

--- tart/actuators/hbridge.py
+++ tart/actuators/hbridge.py
@@ -4,18 +4,46 @@ from tart.arduino import arduino
 
 class HMotor:
     def __init__(self, ard, direction_port, brake_port, pwm_port):
-        direction = arduino.DigitalOut(ard, direction_port)
-        brake = arduino.DigitalOut(ard, brake_port)
-        pwm = arduino.PWM(ard, pwm_port)
+        self.direction = arduino.DigitalOut(ard, direction_port)
+        self.brake = arduino.DigitalOut(ard, brake_port)
+        self.pwm = arduino.PWM(ard, pwm_port)
 
     def setValue(self, value): # between -127 and 127
         if value >= 0:
-            direction.setValue(1)
+            self.direction.setValue(1)
         else:
-            direction.setValue(0)
-        brake.setValue(0)
-        pwm.setValue(abs(value/127.))
+            self.direction.setValue(0)
+        self.brake.setValue(0)
+        self.pwm.setValue(abs(value/127.))
 
     def brake(self, value=1): # between 0 (coast) and 1 (brake)
-        brake.setValue(1)
-        pwm.setValue(value)
+        self.brake.setValue(1)
+        self.pwm.setValue(value)
+
+if __name__=="__main__":
+    try:
+        ard = arduino.ArduinoThread(debug=True)
+        motor = HMotor(ard, 22, 23, 24)
+        #sensor = AnalogSensor(ard, 0)
+        #servo = Servo(ard, 8)
+        #transistor = Transistor(ard, 12)
+
+        ard.start()
+        assert ard.waitReady()
+
+        motor.setValue(127)
+        #transistor.setValue(1)
+        for i in range(100):
+            #print sensor.getValue()
+            #servo.setAngle(i)
+            time.sleep(0.1)
+        #servo.setAngle(0)
+        #transistor.setValue(0)
+        time.sleep(1)
+        
+    #This is so that when you hit ctrl-C in the terminal, all the arduino threads close. You can do something similar with threads in your program.
+    except KeyboardInterrupt:
+        print "Ending Program"
+    
+    finally:
+        ard.stop()
