968fc4ccdc17de5a07e99bfccc175e0ad987ff35
Anthony <lua@lua-Aspire-5745G.(none)>
2012-02-02 01:53:30 -0500

    Calibrate IR sensor

--- tart/sensors/sensor.py
+++ tart/sensors/sensor.py
@@ -1,3 +1,5 @@
+import sys, time
+sys.path.append("/home/maslab-team-5/Maslab/tart/Libraries/")
 from tart.arduino import arduino
 
 class BumpSensor(arduino.DigitalSensor):
@@ -8,9 +10,24 @@ class BumpSensor(arduino.DigitalSensor):
         return self.getValue() == 0
 
 class ShortIR(arduino.AnalogSensor):
-	"""Short IR wrapper class"""
+    """Short IR wrapper class"""
 
-	def get_dist(self):
-		"""Returns a distance in inches"""
+    def get_dist(self):
+        """Returns a distance in inches"""
         val = self.getValue()
-		raise NotImplementedError
+        return 1./(0.186*val+0.032)
+
+if __name__ == "__main__":
+    try:
+        ard = arduino.Arduino()
+        bump = BumpSensor(ard, 8)
+        ir = ShortIR(ard, 8)
+
+        ard.start()
+        ard.waitReady()
+        
+        for i in range(100):
+            print ir.get_dist()
+            time.sleep(0.1)
+    finally:
+        ard.stop()
