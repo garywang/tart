b082c79daf62cf6762eac410bbe489d6a4548c66
Anthony <lua@lua-Aspire-5745G.(none)>
2012-02-01 22:15:01 -0500

    Added IR and wall following

---
+++ tart/control/wall_follow.py
@@ -0,0 +1,39 @@
+import sys, time, math
+import threading, thread
+from tart.actuators import drive
+from tart.sensors import sensor
+from tart import params
+
+class WallFollowController:
+    def __init__(self, ard, drive, back_port, side_port):
+        """Follows a wall.
+        
+        Takes port numbers for two short IR sensors: back, facing the back of the robot; and side, facing 60 degrees from the back."""
+        self.drive = drive
+        self.back = sensors.ShortIR(ard, back_port)
+        self.side = sensors.ShortIR(ard, side_port)
+
+    def follow_wall(self):
+        """Uses a simple form of control to follow walls.
+        
+        If its back is to a wall, it drives right along the wall. Otherwise, it drives backward until it hits a wall."""
+        if self.at_wall():
+            self.track_wall()
+        else:
+            self.find_wall()
+
+    def at_wall(self):
+        back_dist = self.back.get_dist()
+        return back_dist < 8
+
+    def track_wall(self):
+        side_dist = self.side.get_dist()
+        if side_dist < 15: # Too close
+            self.drive.rotate(-30)
+        elif side_dist > 25: # Too far
+            self.drive.rotate(30)
+        else:
+            self.drive.translate(127, 90)
+
+    def find_wall(self): # Possibly might need to be more involved.
+        self.drive.translate(127, 180)
--- tart/sensors/sensor.py
+++ tart/sensors/sensor.py
@@ -6,3 +6,11 @@ class BumpSensor(arduino.DigitalSensor):
     def pressed(self):
         """Returns True if pressed, False otherwise"""
         return self.getValue() == 0
+
+class ShortIR(arduino.AnalogSensor):
+	"""Short IR wrapper class"""
+
+	def get_dist(self):
+		"""Returns a distance in inches"""
+        val = self.getValue()
+		raise NotImplementedError
